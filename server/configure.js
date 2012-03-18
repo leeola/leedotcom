783a4f92d39fe2a357b3d6e2963555093bd1cb71
Lee Olayvar <leeolayvar@gmail.com>
2012-03-18 03:17:34 +0000

    Added stylus, and some base stylus css files.

--- server/configure.js
+++ server/configure.js
@@ -6,25 +6,53 @@
 //
 /*jshint asi: true*/
 var express = require('express')
+  , stylus = require('stylus')
 var app = require('./app')
 
 
 app.configure(function () {
-  app.use(express.logger())
+  console.log('Generic')
   app.set('views', __dirname +'/views')
   app.set('view engine', 'jade')
-  app.set('view options', {'layout': false})
+  
+  app.use(express.logger())
 })
 
 app.configure('development', function () {
+  console.log('Development')
   app.set('port', process.env.PORT)
+  app.set('view options', {
+    'layout': false,
+    'pretty': true
+  })
+  
   app.use(express.errorHandler({
     dumbExceptions: true,
     showStack: true
+  }))
+  app.use(stylus.middleware({
+    // Force our stylus files to always recompile.
+    'force': true,
+    'linenos': true,
+    'src': __dirname +'/../public'
   }))
 })
 
 app.configure('production', function () {
   app.set('port', 80)
+  app.set('view options', {'layout': false})
+  
   app.use(express.errorHandler())
+  app.use(stylus.middleware({
+    'compress': true,
+    'src': __dirname +'/../public'
+  }))
+})
+
+// This 2nd 'plain' configure call is here so that we can call some
+// middleware after others. Namely static, after stylus, as calling them
+// the other way around was causing stylus to have issues refreshing
+// content.
+app.configure(function () {
+  app.use(express.static(__dirname +'/../public'))
 })
