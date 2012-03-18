b3d9bdf6a55f7e8d5220f1e9b22364cb5c888486
Lee Olayvar <leeolayvar@gmail.com>
2012-03-18 21:31:28 +0000

    Added some basic js link intervention code. Yay for pushState!

--- client/main.js
+++ client/main.js
@@ -5,5 +5,26 @@
 // @copyright 2012 by Lee Olayvar
 //
 /*jshint asi: true*/
+var jquery = require('jquery-browserify')
 
-console.log('Hello!')
+
+var main = function () {
+  var processEvent = function (event) {
+    console.log('Event! '+ event)
+    event.preventDefault()
+    
+    var link_title = event.target.innerHTML
+    var link_href = event.target.getAttribute('href')
+    history.pushState(null, link_title, link_href)
+  }
+  
+  var els = document.getElementsByTagName('a')
+  
+  for (var i = 0; i < els.length; i++) {
+    els[i].addEventListener('click', processEvent, false)
+  }
+}
+
+jquery(function () {
+    main()
+})
